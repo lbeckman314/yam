c69919666a3eac40f12f5a76a3b2dd02a93821d7
Liam Beckman <lbeckman@uoregon.edu>
2016-08-01 13:10:39 -0700

    Create bucky 9 increment operators.java

---
+++ bucky 9 increment operators.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class apples
+{
+  public static void main(String[] args)
+  {
+    Scanner spiffy = new Scanner(System.in);
+    
+    double yam = 9;   //++yam = 10, yam++ = 9, yam += 5 == 14 ---- *= /= %=
+    double may = 29;  //--may = 28, may-- =29, may -= 5 == 24 ----*= /= %=
+    
+    System.out.println(++yam);
+  }
+}
