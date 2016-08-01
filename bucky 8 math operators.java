a24125981284594d95272f6332747f0d459a6c15
Liam Beckman <lbeckman@uoregon.edu>
2016-08-01 13:04:47 -0700

    Create bucky 8 math operators.java

---
+++ bucky 8 math operators.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class apples
+{
+  public static void main(String[] args)
+  {
+    Scanner yam = new Scanner(System.in);
+    
+    double girls, boys, people;
+    girls = 9;
+    boys = 27;
+    people = girls + boys;    //Other operators : - * / %
+    
+    System.out.println("The number of people are:" + people);
+  }
+}
